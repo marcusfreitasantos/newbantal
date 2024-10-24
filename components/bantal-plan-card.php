2a3eaa04944eada9892db484ca6476275da548d7
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-10-24 07:37:52 -0300

    [FIX] Bantal play logo

--- components/bantal-plan-card.php
+++ components/bantal-plan-card.php
@@ -4,7 +4,7 @@ function BantalPlanCard(){
 
      ?>
         <div class="bantal__plans_card">
-            <img src="http://localhost/newbantal/wp-content/uploads/2024/10/bantal-play.svg" alt="plan-logo" />
+            <img src="https://newbantal.mafreitas.com.br/wp-content/uploads/2024/10/bantal-play.svg" alt="plan-logo" />
             <span class="bantal__plans_price">R$0,00</span>
             <span class="bantal__plans_period">Gratuito</span>
             <?= ButtonLink('/', 'Escolher', 'secondary', 'blank'); ?>
