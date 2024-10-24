75409ebd118c49f3fd76cf9c43e48264bed4121b
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-10-23 23:38:30 -0300

    [FEAT] Warning btn css adjustment

--- components/bantal-plan-card.php
+++ components/bantal-plan-card.php
@@ -7,7 +7,7 @@ function BantalPlanCard(){
             <img src="http://localhost/newbantal/wp-content/uploads/2024/10/bantal-play.svg" alt="plan-logo" />
             <span class="bantal__plans_price">R$0,00</span>
             <span class="bantal__plans_period">Gratuito</span>
-            <?= ButtonLink('/', 'Plano atual', 'primary', 'blank', true); ?>
+            <?= ButtonLink('/', 'Escolher', 'secondary', 'blank'); ?>
 
             <div class="bantal__plans_card_details_btn">
                 <span>Ver detalhes</span>
