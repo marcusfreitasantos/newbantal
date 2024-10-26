2092738be61484fa805ee419a6fa9c73487dcdff
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-10-25 21:41:34 -0300

    [FIX] Changed form ID

--- page-contato.php
+++ page-contato.php
@@ -13,7 +13,7 @@
     
                 <div class="col-md-6 p-5">
                    <h1 class="page__title_invert"><?php echo the_title(); ?></h1>
-                   <?php echo do_shortcode('[contact-form-7 id="cf9d0f3" title="Formulário de contato 1"]'); ?>
+                   <?php the_content(); ?>
                 </div>
     
             </div>
