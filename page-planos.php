c5e1cfa85981711ca666c75f64ab6d9cbae5dc76
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-10-29 22:52:48 -0300

    [FEAT] Dynamic display for plans based on catregory

--- page-planos.php
+++ page-planos.php
@@ -23,6 +23,10 @@ $bannerBg = get_field("banner_background");
         background-repeat: no-repeat;
         text-align: center;
     }
+
+    .annual__plans{
+        display: none;
+    }
 </style>
 
 
@@ -35,19 +39,44 @@ $bannerBg = get_field("banner_background");
             </div>
         </div>
     </div>
+
+    <div class="plan__category_btn_wrapper">
+        <button class="plan__category_btn active" id="plan__category_month">Mensal</button>
+        <button class="plan__category_btn" id="plan__category_year">Anual</button>
+    </div>
 </section>
+
+
 
 <section class="bantal__plans_section_cards py-2">
     <div class="bantal__plans_cards_container">
-        <div class="row">
+
+        <div class="row justify-content-center month__plans">
             <?php if($getCurrentPlans->have_posts()){
                 while($getCurrentPlans->have_posts()):
                     $getCurrentPlans->the_post();
                     global $post;
+                    if(has_term("mensal", "plan_category", $post->id)){ ?>
+                        <div class="col-md-2 mb-3">
+                            <?= BantalPlanCard($post); ?>
+                        </div> 
+                    <?php }
                     ?>
-                    <div class="col-md-2 mb-3">
-                        <?= BantalPlanCard($post); ?>
-                    </div> 
+                <?php endwhile; ?>
+            <?php } ?>
+        </div>
+
+        <div class="row justify-content-center annual__plans">
+            <?php if($getCurrentPlans->have_posts()){
+                while($getCurrentPlans->have_posts()):
+                    $getCurrentPlans->the_post();
+                    global $post;
+                    if(has_term("anual", "plan_category", $post->id)){ ?>
+                        <div class="col-md-2 mb-3">
+                            <?= BantalPlanCard($post); ?>
+                        </div> 
+                    <?php }
+                    ?>
                 <?php endwhile; ?>
             <?php } ?>
         </div>
@@ -73,5 +102,22 @@ $bannerBg = get_field("banner_background");
             planResources[index].classList.remove("show-resources")
         })
     })
+
+    const btnCategoryPlan = [...document.querySelectorAll(".plan__category_btn")];
+    btnCategoryPlan.map((categoryBtn) => {
+        categoryBtn.addEventListener("click", function(e){
+            
+            if(e.currentTarget.id === "plan__category_month"){
+                document.querySelector(".month__plans").style.display = "flex";
+                document.querySelector(".annual__plans").style.display = "none";
+            }else{
+                document.querySelector(".month__plans").style.display = "none";
+                document.querySelector(".annual__plans").style.display = "flex";
+            }
+        })
+    })
+
+
+
 </script>
 <?php get_footer();?>
