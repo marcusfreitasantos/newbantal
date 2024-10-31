8ca3246b32f5e9939ae854e74a60815dc27c8a6f
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-10-31 17:32:24 -0300

    [FEAT] Show custom markers on the map

--- components/google-maps.php
+++ components/google-maps.php
@@ -3,6 +3,7 @@ function GoogleMaps(){ ?>
 <?php 
 $googleApiKey = GOOGLE_MAPS_API_KEY; 
 $childThemeDirectory = get_stylesheet_directory_uri();
+$defaultUserAvatar = "$childThemeDirectory/assets/img/favicon.png";
 ?>
 
     <!-- prettier-ignore -->
@@ -13,57 +14,81 @@ $childThemeDirectory = get_stylesheet_directory_uri();
         let currentUserLat = 0;
         let currentUserLong = 0;
 
-        if("geolocation" in navigator)
-        
+        if ("geolocation" in navigator)
         navigator.geolocation.getCurrentPosition((position) => {
             currentUserLat = position.coords.latitude;
             currentUserLong = position.coords.longitude;
             initMap();
-
         });
 
         async function getAllUsers() {
-            try {
-                const allUsers = await callAjaxGetUsers();
-                return allUsers;
-            } catch (error) {
-                console.error("Error fetching users:", error);
-            }
+        try {
+            const allUsers = await callAjaxGetUsers();
+            return allUsers;
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
         }
 
         async function initMap() {
-            const { Map } = await google.maps.importLibrary("maps");
-            const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+        const { Map } = await google.maps.importLibrary("maps");
+        const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+            "marker"
+        );
 
-            let map = new Map(document.getElementById("bantal__custom_map"), {
-                center: { lat: currentUserLat, lng: currentUserLong },
-                zoom: 12,
-                mapId: "bantal__custom_map",
-            });
+        let map = new Map(document.getElementById("bantal__custom_map"), {
+            center: { lat: currentUserLat, lng: currentUserLong },
+            zoom: 14,
+            mapId: "bantal__custom_map",
+            disableDefaultUI: true,
+        });
 
-            const marker = new AdvancedMarkerElement({
-                map,
-                position: { lat: currentUserLat, lng: currentUserLong },
-            });
+        const currentUserPin = new PinElement({
+            scale: 2,
+            background: "#f0903a",
+            borderColor: "#f0903a",
+        });
 
-            const allUsers = await getAllUsers();
+        const marker = new AdvancedMarkerElement({
+            map,
+            position: { lat: currentUserLat, lng: currentUserLong },
+            content: currentUserPin.element,
+        });
+
+        const allUsers = await getAllUsers();
+
+        const buildContent = (userAvatar) => {
+            const bantalUsersAvatar = document.createElement("div");
+            bantalUsersAvatar.innerHTML = `
+            <div class='bantal__map_user_avatar_wrapper'>
+                <img src='${userAvatar}' />
+            </div>
+            `
+            return bantalUsersAvatar;
+        }
 
             allUsers.map((bantalUser) => {
-                if(bantalUser.latitude && bantalUser.longitude){
+                if (bantalUser.latitude && bantalUser.longitude) {
+                    const avatarBase64Url = `data:image/png; base64, ${bantalUser.photo}`;
+
                     const bantalUserMarker = new AdvancedMarkerElement({
                         map,
-                        position: { lat: Number(bantalUser.latitude), lng: Number(bantalUser.longitude) },
-                    }); 
+                        position: {
+                        lat: Number(bantalUser.latitude),
+                        lng: Number(bantalUser.longitude),
+                        },
+                        content: buildContent(bantalUser.photo ? avatarBase64Url : "<?= $defaultUserAvatar; ?>")
+                    });
 
-                    bantalUserMarker.addListener('click', ({domEvent, latLng}) => {
-                        const {target} = domEvent;
-                        window.location.href = "https://recrutamento.bantal.com.br/recrutamento"
+                    bantalUserMarker.addListener("click", ({ domEvent, latLng }) => {
+                        const { target } = domEvent;
+                        window.location.href =
+                        "https://recrutamento.bantal.com.br/recrutamento";
                     });
                 }
-            })
+            });
         }
     </script>
-
 
     <div id="bantal__custom_map" style="height: 600px"></div>
 <?php }
